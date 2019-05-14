39604083bb3e5093f0d470d042ab5b3c25303def
jimbits <contactjamesfoley@gmail.com>
2019-05-14 09:21:37 -0600

    using popular librarirs to animate pages and components

---
+++ react-animation/react-animation-style-slider/build/precache-manifest.7a1904a62cc3a6d8e8e2729936df3ab7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c56f2b579b2e29d9f37720121c00181f",
+    "url": "//192.168.1.67/react/react-animation/react-animation-style-slider/build/static/media/nait-grey-scale.c56f2b57.svg"
+  },
+  {
+    "revision": "1118b02a93468c4c7aa0",
+    "url": "//192.168.1.67/react/react-animation/react-animation-style-slider/build/static/js/runtime~main.1118b02a.js"
+  },
+  {
+    "revision": "f9f6424a3f77c1163c60",
+    "url": "//192.168.1.67/react/react-animation/react-animation-style-slider/build/static/js/main.f9f6424a.chunk.js"
+  },
+  {
+    "revision": "25290728e4ed51d7fe02",
+    "url": "//192.168.1.67/react/react-animation/react-animation-style-slider/build/static/js/2.25290728.chunk.js"
+  },
+  {
+    "revision": "f9f6424a3f77c1163c60",
+    "url": "//192.168.1.67/react/react-animation/react-animation-style-slider/build/static/css/main.40c8dc82.chunk.css"
+  },
+  {
+    "revision": "afadec0c4f52770e66abb20d9f8b2233",
+    "url": "//192.168.1.67/react/react-animation/react-animation-style-slider/build/index.html"
+  }
+];
---
+++ react-animation/react-animation-style-slider/build/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "//192.168.1.67/react/react-animation/react-animation-style-slider/build/precache-manifest.7a1904a62cc3a6d8e8e2729936df3ab7.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("//192.168.1.67/react/react-animation/react-animation-style-slider/build/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
